ab4e890193b8a064b7dcb9e1a73a48dafc9c7f08
m.couderc <m.couderc@komet-is.fr>
2023-06-13 08:54:48 +0200

    fix: imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RedlightComponent } from './redlight/redlight.component';
-import { BumperComponent } from './bumper/bumper.component';
-import { HoodComponent } from './hood/hood.component';
+import { RedlightComponent } from './landing-page/redlight/redlight.component';
+import { BumperComponent } from './landing-page/bumper/bumper.component';
+import { HoodComponent } from './landing-page/hood/hood.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { CarComponent } from './car/car.component';
 
